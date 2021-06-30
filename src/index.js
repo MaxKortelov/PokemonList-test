4fbd4afc42dab0304a7ef1f45ab1b05382571b11
Max Kortelov <kortelevmax@fromzerotowebhero.ru>
2021-07-01 00:22:54 +0300

    installed additional typescript and mobx libraries to project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.sass';
-import App from './App';
+import App from './App.tsx';
 
 ReactDOM.render(
   <React.StrictMode>
