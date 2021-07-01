427b372272d6989f544c4427445c99a3c68d9b2a
Max Kortelov <kortelevmax@fromzerotowebhero.ru>
2021-07-02 01:41:32 +0300

    modified CardPokemon

--- src/Components/CardPokemon/index.tsx
+++ src/Components/CardPokemon/index.tsx
@@ -1,13 +1,13 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import Skeleton from '@material-ui/lab/Skeleton';
 
 import styles from './CardPokemon.module.sass';
+import axios from 'axios';
 
 interface ICard {
     info: {
@@ -16,36 +16,69 @@ interface ICard {
     },
 }
 
+interface IPokemon {
+    sprites: {
+        other: {
+            'official-artwork': {
+                'front_default': string
+            }
+        }
+    },
+    types: [
+        {
+            type: {
+                name: string
+            }
+        }
+    ]
+}
+
 const CardPokemon : FC<ICard> = ({info}) => {
+
+    const [loading, setLoading] = useState(true);
+    const [pokemon, setPokemon] = useState<IPokemon | {}>({});
+
+    useEffect(() => {
+        axios.get(info.url)
+            .then(res => {
+                if(res.status === 200) {
+                    setPokemon(res.data);
+                    setLoading(false);
+                    console.log(res.data);
+                }
+            })
+            .catch(err => console.log(err))
+    }, [info.url]);
+
+    const renderTypes = () => {
+        return 'types' in pokemon ? pokemon.types.map(type => {
+            return <div className={styles.type}>{type.type.name[0].toUpperCase() + type.type.name.substring(1)}</div>
+        }) : null;
+    };
+
     return(
         <div className={styles.cardListWrap}>
             <Card>
                 <CardActionArea>
-                    <CardMedia
-                        component="img"
-                        alt="Contemplative Reptile"
-                        height="140"
-                        image="./assets/preview.png"
-                        title="Contemplative Reptile"
-                    />
+                    {loading
+                    ? <Skeleton variant="rect" width="100%">
+                        <div style={{ paddingTop: '57%' }} />
+                    </Skeleton>
+                    : <CardMedia
+                        component='img'
+                        alt='Pokemon'
+                        height='140'
+                        image={'sprites' in pokemon ? pokemon.sprites.other['official-artwork']['front_default'] : ''}
+                    />}
                     <CardContent>
-                        <Typography gutterBottom variant="h5" component="h2">
-                            Lizard
+                        <Typography gutterBottom variant='h5' component='h2'>
+                            {info.name.toUpperCase()}
                         </Typography>
-                        <Typography variant="body2" color="textSecondary" component="p">
-                            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                            across all continents except Antarctica
-                        </Typography>
+                        <div className={styles.typesWrap}>
+                            {renderTypes()}
+                        </div>
                     </CardContent>
                 </CardActionArea>
-                <CardActions>
-                    <Button size="small" color="primary">
-                        Share
-                    </Button>
-                    <Button size="small" color="primary">
-                        Learn More
-                    </Button>
-                </CardActions>
             </Card>
         </div>
     )
