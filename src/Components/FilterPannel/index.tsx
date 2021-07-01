5df23ab519c20884be26f36dc281371543326932
Max Kortelov <kortelevmax@fromzerotowebhero.ru>
2021-07-01 19:40:31 +0300

    added FilterPannel

---
+++ src/Components/FilterPannel/index.tsx
@@ -0,0 +1,13 @@
+import React, {FC} from 'react';
+
+import styles from './FilterPannel.module.sass';
+
+const FilterPannel : FC = () => {
+    return (
+        <div className={styles.filterPannelWrap}>
+            123
+        </div>
+    )
+}
+
+export default FilterPannel;
