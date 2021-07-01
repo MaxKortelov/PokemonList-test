00d5980f0b47c907d7b77c5b8f3258a30064ee98
Max Kortelov <kortelevmax@fromzerotowebhero.ru>
2021-07-01 19:38:48 +0300

    added general request for Pokemons in Store

---
+++ src/Store/PokemonInfo.tsx
@@ -0,0 +1,34 @@
+import {action, makeAutoObservable, observable} from 'mobx';
+import axios from "axios";
+
+interface IPokemonList {
+    count: number,
+    next: string,
+    previous: string,
+    results: object
+}
+
+class Pokemon{
+    @observable pokemonList?: IPokemonList | {} = {};
+    @observable entered: boolean = false;
+
+    constructor() {
+        makeAutoObservable(this)
+    }
+
+    @action
+    addPokemon() {
+        const url = `https://pokeapi.co/api/v2/pokemon/?limit=60&offset=60`
+        axios.get(url)
+            .then(res => {
+                if(res.status === 200) {
+                    this.pokemonList = {...res.data};
+                }
+            })
+            .catch(err => console.log(err));
+    }
+    @action
+    toggleEntered(boolean: boolean) {this.entered = boolean}
+}
+
+export default new Pokemon();
