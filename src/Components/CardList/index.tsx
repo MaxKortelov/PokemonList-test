2d8cc2421affe0a66fc07ed6d6688f7a33b59e29
Max Kortelov <kortelevmax@fromzerotowebhero.ru>
2021-07-01 23:00:46 +0300

    added CardList

---
+++ src/Components/CardList/index.tsx
@@ -0,0 +1,12 @@
+import React, {FC} from 'react';
+
+import styles from './CardList.module.sass';
+
+const CardList : FC = () => {
+    return(
+        <div className={styles.cardListWrap}>
+        </div>
+    )
+}
+
+export default CardList;
